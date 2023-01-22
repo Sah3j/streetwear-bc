5cc1cababd39d60e72367673b2d5fa6ee2b0f764
Sahejdeep Singh <bboysahej@gmail.com>
2023-01-21 16:25:55 -0800

    gh-pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import LoginPage from "./LoginPage.js";
 import LoginForm from "./LoginForm.js";
 import SignUpForm from "./SignUpForm.js"
-import {Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import { UserAuthContextProvider } from './context/UserAuthContext';
 import ProtectedRoute from "./ProtectedRoute.js"
 import ExplorePage from './ExplorePage.js';
@@ -16,18 +16,20 @@ function App() {
   return (
     <div className="App">
       <UserAuthContextProvider>
-        <Routes basename={window.location.pathname || ''}>
-          <Route path="/" element={<LoginPage />} >
-            <Route index element={<LoginForm />} />
-            <Route path="signup" element={<SignUpForm />} />
-          </Route>
-          <Route path="profile-info" element={<NameAndPicPage/>} />
-          <Route path="explore" element={<ProtectedRoute> <ExplorePage/> </ProtectedRoute>} >
-            <Route index element={<ProtectedRoute> <Products /> </ProtectedRoute>} />
-            <Route path="profile" element={<ProtectedRoute> <ProfilePage /> </ProtectedRoute>} />
-            <Route path="setting" element={<ProtectedRoute> <SettingsPage /> </ProtectedRoute>} />
-          </Route>
-        </Routes>
+        <BrowserRouter basename="streetwear-bc">
+          <Routes>
+            <Route path="/" element={<LoginPage />} >
+              <Route index element={<LoginForm />} />
+              <Route path="signup" element={<SignUpForm />} />
+            </Route>
+            <Route path="profile-info" element={<NameAndPicPage/>} />
+            <Route path="explore" element={<ProtectedRoute> <ExplorePage/> </ProtectedRoute>} >
+              <Route index element={<ProtectedRoute> <Products /> </ProtectedRoute>} />
+              <Route path="profile" element={<ProtectedRoute> <ProfilePage /> </ProtectedRoute>} />
+              <Route path="setting" element={<ProtectedRoute> <SettingsPage /> </ProtectedRoute>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>      
       </UserAuthContextProvider>
     </div>
   );
